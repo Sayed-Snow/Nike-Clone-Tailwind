8d2bc7680457401d665613fdbb8b182fd6e35b93
Sayed-Snow <sayedpeters00@gmail.com>
2023-09-28 13:54:18 +0200

    Set up for clone

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App = () => (
+  <main className="'relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b ">
+      Hero
+    </section>
+    <section className="padding">
+      Popular Product
+    </section>
+    <section className="padding">
+      Super Quailty
+    </section>
+    <section className="padding-x py-10">
+      Servers
+    </section>
+    <section className="padding">
+      Special Offer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Sub
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+  </main>
+);
+
+export default App
